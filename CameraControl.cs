5af649548cb8c235fe420b63996bd0444f6ee301
yhkk2585 <yhkk2585@naver.com>
2015-10-29 12:26:46 +0900

    Unity Control C#

---
+++ CameraControl.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+	
+	Vector3 rotationRate;
+	Vector3 angle;
+	Vector3 startAngle;
+	// Vector3 startDualAngle;
+	public GameObject camera;
+	//  public GameObject dualCamera;
+	float ROTATION_SPEED = 0.1f;
+	float startAngleX, startAngleY,startAngleZ;
+	void Start()
+	{
+		Input.gyro.enabled = true;
+		startAngle = camera.transform.localEulerAngles;
+		//   startDualAngle = dualCamera.transform.localEulerAngles;
+		
+	}
+	void Update()
+	{
+		
+		if ((Mathf.Abs(Input.gyro.rotationRateUnbiased.y) > ROTATION_SPEED) )
+		{
+			rotationRate = Input.gyro.rotationRateUnbiased;
+			angle.y += -rotationRate.y * 3;
+			angle.z = 0;
+			camera.transform.localEulerAngles = angle;
+			// dualCamera.transform.localEulerAngles = angle;
+			
+			
+		}
+		if ((Mathf.Abs(Input.gyro.rotationRateUnbiased.x) > ROTATION_SPEED))
+		{
+			rotationRate = Input.gyro.rotationRateUnbiased;
+			angle.x += -rotationRate.x * 3;
+			angle.z = 0;
+			camera.transform.localEulerAngles = angle;
+			// dualCamera.transform.localEulerAngles = angle;
+			
+		}
+		if (Input.GetButtonDown("StartPoint"))
+		{
+			camera.transform.localEulerAngles = startAngle;
+			// dualCamera.transform.localEulerAngles = startDualAngle;
+			angle.x =startAngle.x;
+			angle.y =startAngle.y;
+		}
+		
+	}
+	void OnGUI()
+	{
+		GUILayout.TextArea("rotation y : " + Input.gyro.rotationRateUnbiased.y + "\nrotation x : " + Input.gyro.rotationRateUnbiased.x+"\nrotaion z: "+Input.gyro.rotationRateUnbiased.z);
+		GUILayout.TextArea("rotation x : " +Input.gyro.userAcceleration.x);
+		
+	}
+}
+
---
+++ moving.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moving : MonoBehaviour
+{
+	public float speed = 5.0f;        // moving speed
+	public float rotSpeed = 120.0f; // rotation speed
+	public GameObject turret;       // tank turret object
+	public bool test;
+	
+	public int power = 2000;  // bullet speed power
+	public Transform bullet;  // bullet object
+	void Update()
+	{
+		float amtToMove = speed * Time.deltaTime;  // moving distance per frame
+		float amtToRot = rotSpeed * Time.deltaTime;  // rotation angle per frame
+		
+		float front = Input.GetAxis("Vertical");          // get input information (moving front/back)
+		float ang = Input.GetAxis("Horizontal");       // get input information (moving left/right)
+		
+		transform.Translate(Vector3.forward * front * amtToMove); // moving front/back
+		transform.Rotate(new Vector3(0, ang * amtToRot, 0));        // moving left/right
+		
+		
+	}
+}
+
